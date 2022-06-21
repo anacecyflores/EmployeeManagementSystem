92723b63177d4b72cdda40694e54795d8f51239f
Cecy Flores <anacecyflores1@gmail.com>
2022-06-20 23:48:52 -0500

    Inserted the Employee, department and roles data into tables

--- employee_db/seeds.sql
+++ employee_db/seeds.sql
@@ -1,20 +1,28 @@
-INSERT INTO employee (first_name, last_name, id, role_id, manager_id)
-VALUES ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", );
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lord", "Farquad",1,NULL ),
+       ("Elliot", "Smith" ,3,NULL),
+       ("Frida ", "Kahlo",2 ,NULL ),
+       ("Gabriel", "Garcia-Marquez", 4,NULL ),
+       ("Isabel", "Allende",4, NULL),
+       ("Danny ", "De Vito", 5, 1),
+       ("Jimmi", "Hendrix",3, NULL),
+       ("Leonardo", " Da Vinci" ,2,2);
 
-       INSERT INTO department (id, department_name)
-VALUES ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", );
+       INSERT INTO department ( department_name)
+VALUES( "movies" ),
+       ( "art"),
+       ( "music" ),
+       ( "books" ),
+       ( "books" ),
+       ( "entertainment" ),
+       ( "music" ),
+       ( "Management" );
 
-       INSERT INTO roles (id, title, salary, department_id)
-VALUES ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", );
+       INSERT INTO roles ( title, salary, department_id)
+VALUES ( "villain", 500000, 1),
+       ( "artist", 20000, 2 ),
+       ( "writer", 45000, 3),
+       ( "musician", 50000, 3),
+       ( "writer", 45000, 4 ),
+       ( "manager", 100000, 5 ),
+       ( "manager", 100000, 5);
