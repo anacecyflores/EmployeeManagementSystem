51968154241c17f807ab852565f2a01e959c631a
Cecy Flores <anacecyflores1@gmail.com>
2022-06-20 21:08:51 -0500

    Inserted the Employee, Department and Role format into the seeds.sql

--- employee_db/schema.sql
+++ employee_db/schema.sql
@@ -6,22 +6,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE employee (
-  
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 
-  name VARCHAR(100) NOT NULL
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
 );
 
 CREATE TABLE department (
-  
-  id INT NOT NULL,
+  id INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
 
-  name VARCHAR(100) NOT NULL
+  department_name VARCHAR(30) NOT NULL
 );
 -- changed from role to roles to abide by the syntax rules
 CREATE TABLE roles (
-  
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 
-  name VARCHAR(100) NOT NULL
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT
 );
---
+++ employee_db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO employee (first_name, last_name, id, role_id, manager_id)
+VALUES ("", "", ),
+       ("", "", ),
+       ("", "", ),
+       ("", "", ),
+       ("", "", );
+
+       INSERT INTO department (id, department_name)
+VALUES ("", "", ),
+       ("", "", ),
+       ("", "", ),
+       ("", "", ),
+       ("", "", );
+
+       INSERT INTO roles (id, title, salary, department_id)
+VALUES ("", "", ),
+       ("", "", ),
+       ("", "", ),
+       ("", "", ),
+       ("", "", );
