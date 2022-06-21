5e73d25b538775e4d05d37b3580aa1e064525ebe
Cecy Flores <anacecyflores1@gmail.com>
2022-06-21 10:38:15 -0500

    Created 1st Query for roles

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 // get the client
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const consTable = requeire("console.table");
+const consTable = require("console.table");
 // create the connection to database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "runSql",
-  //   database: "test",
+  password: "1234",
+  database: "employee_db",
 });
-con.connect(function (err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
-// simple query
-connection.query(
-  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-  function (err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
-// with placeholder
-connection.query(
-  "SELECT * FROM `table` WHERE `name` = ? AND `age` > ?",
-  ["Page", 45],
-  function (err, results) {
+
+showRoles();
+
+function showRoles() {
+  const sql = "SELECT * FROM roles";
+  connection.query(sql, function (err, results) {
+    if (err) throw err;
     console.log(results);
-  }
-);
+    console.table(results);
+  });
+}
+// with placeholder
+// connection.query(
+//   "SELECT * FROM `table` WHERE `name` = ? AND `age` > ?",
+//   ["Page", 45],
+//   function (err, results) {
+//     console.log(results);
+//   }
+// );
