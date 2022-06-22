f61930794284f8c8dc8bddbb3a2c3feced5f85dc
Cecy Flores <anacecyflores1@gmail.com>
2022-06-22 09:57:40 -0500

    Add Role Function Successfuly console logged

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const ct = require("console.table");
 
-// create the connection to database
+// Connection to database-----------------------
 const con = mysql.createConnection({
   host: "localhost",
   user: "runSql",
@@ -14,7 +14,7 @@ con.connect(function (err) {
   if (err) throw err;
   console.log("Wow!");
 });
-
+// ----------------Start Inquirer prompt-----
 init();
 function init() {
   inquirer
@@ -59,7 +59,7 @@ function init() {
       }
     });
 }
-
+// --------------Show Employee Function--------
 function showEmployees() {
   const sql = "SELECT * FROM employee";
   con.query(sql, function (err, result) {
@@ -68,7 +68,7 @@ function showEmployees() {
     console.table(result);
   });
 }
-
+// --------------Show Department Function------
 function showDepts() {
   const sql = "SELECT * FROM department";
   con.query(sql, function (err, result) {
@@ -77,7 +77,7 @@ function showDepts() {
     console.table(result);
   });
 }
-
+// --------------Show Roles Function------
 function showRoles() {
   const sql = "SELECT * FROM roles";
   con.query(sql, function (err, result) {
@@ -86,9 +86,7 @@ function showRoles() {
     console.table(result);
   });
 }
-
-//-------------------------------------------------------------------------------
-
+//-------------ADD Employee Function----------------
 function addEmp() {
   const sqlR = "SELECT * FROM roles";
   con.query(sqlR, function (err, result) {
@@ -148,15 +146,69 @@ function addEmp() {
       });
   });
 }
-
+// --------------Add Role Function------
 function addRole() {
-  const sql =
-    'INSERT INTO roles (title, salary, department_id) VALUES ("", "", 1)';
+  const sqlArole = "SELECT * FROM department";
+  con.query(sqlArole, function (err, result) {
+    if (err) throw err;
+    const depArr = [];
+    for (i = 0; i < result.length; i++) {
+      console.log(result);
+      depName = result[i].department_name;
+      depId = result[i].id;
+      console.log(depName, depId);
+      depArr.push({ name: depName, value: depId });
+    }
+    console.log(depArr);
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "role_name",
+          message: "Enter the name of the role you want to add:",
+        },
+        {
+          type: "input",
+          name: "role_salary",
+          message: "Enter the role's salary",
+        },
+        {
+          type: "list",
+          name: "department_id",
+          message: "Which Department does this role belong to?",
+          choices: depArr,
+        },
+      ])
+      .then(function (choices) {
+        console.log(choices);
+        // choices.first_name,
+        // choices.last_name,
+        // choices.role_id,
+        // choices.manager_id
+      });
+  });
 }
-function addDept() {
-  const sql = 'INSERT INTO department (name) VALUES ("")';
-}
-function updateRole() {
-  const sql =
-    'UPDATE employee SET first_name =(""), last_name = (""), role_id = 1, manager_id =1';
-}
+
+//   const sql = con.query("INSERT INTO employee SET ?", {
+//     first_name: choices.first_name,
+//     last_name: choices.last_name,
+//     role_id: choices.role_id,
+//     manager_id: choices.manager_id,
+//   });
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.table(result);
+//   });
+// });
+//   const sql =
+//   //     'INSERT INTO roles (title, salary, department_id) VALUES ("", "", 1)';
+//
+// // --------------Add Department Function------
+// function addDept() {
+//   //   const sql = 'INSERT INTO department (name) VALUES ("")';
+// }
+// // --------------UPDATE Role Function------
+// function updateRole() {
+//   //   const sql =
+//   //     'UPDATE employee SET first_name =(""), last_name = (""), role_id = 1, manager_id =1';
+// }
